4572f89aaae868acc21ae394e7bd2f5b46f261c8
vispan77 <vishlpandey2332@gmail.com>
2026-06-17 23:26:43 +0530

    fixed cors

--- server/server.js
+++ server/server.js
@@ -20,9 +20,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
-    origin: [
-        process.env.FRONTEND_URL, "http://localhost:5173"
-    ],
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: [
         "GET", "POST", "PUT", "DELETE"
